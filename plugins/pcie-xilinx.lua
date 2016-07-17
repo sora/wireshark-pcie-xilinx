dcdb5fc111a243c1e94c9c39ffcba35bdd872df6
sora <sora@haeena.net>
2016-07-18 02:06:20 +0900

    update

--- plugins/pcie-xilinx.lua
+++ plugins/pcie-xilinx.lua
@@ -1,36 +1,33 @@
-local pcie_proto = Proto("PCIe", "PCI Express Transport Layer Packet (Xilinx vender format)")
+local pcie_proto = Proto("PCIe_Xilinx", "PCI Express Transport Layer Packet (Xilinx vender format)")
 
 local f = pcie_proto.fields
 
 -- PCIe TLP capture header: Byte 6
 --  2               1             0B
 -- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
--- |   Sequence        |DIR|  TS   |
--- +-+-+-+-+-+-+-+-+-+-+-+-+       |
--- |           Timestamp           |
+-- |DIR|        Reserved           |
+-- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-|
+-- |            Sequence           |
 -- |                               |
 -- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+-- Direction, 2bit
+-- Reserved, 14bit
+-- Sequence, 32bit
 --
--- Version:3bit, default:0
--- Direction:2bit, Host->Dev:00 Dev->Host:01
--- Reserved:3bit, default:0
--- Timestamp:40bit, default:0, Clock_source:PCIe_clock(4ns)
--- 
-f.tcap_ver = ProtoField.new("Version", "pcie.tcap.version", ftypes.UINT8, nil, base.DEC)
---f.tcap_dir  = ProtoField.new("Packet Direction", "pcie.tcap.direction", ftypes.UINT8, nil, base.HEX)
 local TCAPPacketDirection = {
-	[0] = "RX_ENGINE: Completer reQuest (CQ)",
-	[1] = "TX_ENGINE: Completer Completion (CC)",
-	[2] = "TX_ENGINE: Requester reQuest (RQ)",
-	[3] = "RX_ENGINE: Requester Completion (RC)",
+	[0] = "CQ: Completer reQuest (RX_ENGINE)",
+	[1] = "CC: Completer Completion (TX_ENGINE)",
+	[2] = "RQ: Requester reQuest (TX_ENGINE)",
+	[3] = "RC: Requester Completion (RX_ENGINE)",
 }
-f.tcap_dir  = ProtoField.uint8("pcie.tcap.direction", "Packet Direction", base.DEC, TCAPPacketDirection)
-f.tcap_rsvd = ProtoField.new("Reserved", "pcie.tcap.reserved", ftypes.UINT8, nil, base.NONE)
-f.tcap_ts   = ProtoField.new("Timestamp", "pcie.tcap.timestamp", ftypes.BYTES)
+f.tcap_dir  = ProtoField.uint8("pcie.tcap.direction", "Packet Direction", base.BYTES, TCAPPacketDirection)
+-- f.tcap_rsvd = ProtoField.new("Reserved", "pcie.tcap.reserved", ftypes.BYTES)
+f.tcap_sec  = ProtoField.new("packet sequence", "pcie.tcap.sequence", ftypes.UINT32, nil, base.HEX)
 
+--
 -- From Xilinx PG023
 --
--- Completer Request (CQ) Descriptor Format for Memory, I/O, and Atomic Op Requests
+-- CQ: Completer Request Descriptor Format for Memory, I/O, and Atomic Op Requests
 -- |       0       |       1       |       2       |       3       |
 -- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 -- |                       Address [63:32]                         | DW+1
@@ -43,8 +40,85 @@ f.tcap_ts   = ProtoField.new("Timestamp", "pcie.tcap.timestamp", ftypes.BYTES)
 -- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 -- R: Reserved
 -- AT: Address Type
+--
+f.cq_addr  = ProtoField.new("Address", "pcie.cq.addr", ftypes.UINT64, nil, base.HEX)
+f.cq_at    = ProtoField.new("Address Type", "pcie.cq.at", ftypes.UINT8, nil, base.HEX)
+f.cq_rsvd0 = ProtoField.new("Reserved 0", "pcie.cq.rsvd0", ftypes.UINT8, nil, base.NONE)
+f.cq_attr  = ProtoField.new("Attributes", "pcie.cq.attr", ftypes.UINT8, nil, base.HEX)
+f.cq_tc    = ProtoField.new("Transaction Class (TC)", "pcie.cq.tc", ftypes.UINT8, nil, base.HEX)
+f.cq_barap = ProtoField.new("BAR Aperture", "pcie.cq.barap", ftypes.UINT8, nil, base.HEX)
 
--- Completer Completion (CC) Descriptor Format
+local CQ_BARID = {
+	[0] = "BAR 0 (VF-BAR 0 for VFs",
+	[1] = "BAR 1 (VF-BAR 1 for VFs",
+	[2] = "BAR 2 (VF-BAR 2 for VFs",
+	[3] = "BAR 3 (VF-BAR 3 for VFs",
+	[4] = "BAR 4 (VF-BAR 4 for VFs",
+	[5] = "BAR 5 (VF-BAR 5 for VFs",
+	[6] = "Expansion ROM Access",
+	[7] = "No BAR Check (Valid for Root Port only)",
+}
+f.cq_barid = ProtoField.uint8("pcie.cq.barid", "BAR ID", base.BYTES, CQ_BARID)
+
+local CQ_TargetFunction = {
+	[ 0] = "PF0",
+	[ 1] = "PF1",
+	[64] = "VF0",
+	[65] = "VF1",
+	[66] = "VF2",
+	[67] = "VF3",
+	[68] = "VF4",
+	[69] = "VF5",
+}
+f.cq_tf    = ProtoField.uint8("pcie.cq.tf", "Target Function", base.DEC, CQ_TargetFunction)
+f.cq_tag   = ProtoField.new("Tag", "pcie.cq.tag", ftypes.UINT8, nil, base.HEX)
+f.cq_reqid = ProtoField.new("Requester ID", "pcie.cq.reqid", ftypes.UINT16, nil, base.HEX)
+f.cq_rsvd1 = ProtoField.new("Reserved 1", "pcie.cq.rsvd1", ftypes.UINT8, nil, base.NONE)
+
+local CQ_RequestType = {
+	[0] = "Memory Read Request",
+	[1] = "Memory Write Request",
+	[2] = "I/O Read Request",
+	[3] = "I/O Write Request",
+	[4] = "Memory Fetch and Add Request",
+	[5] = "Memory Unconditional Swap Request",
+	[6] = "Memory Compare and Swap Request",
+	[7] = "Locked Read Request (allowed only in Legacy Devices)",
+	[8] = "Type 0 Configuration Read Request (on Requester side only)",
+}
+f.cq_reqtype = ProtoField.uint8("pcie.cq.reqtype", "Request Type", base.BYTES, CQ_RequestType)
+f.cq_dwlen   = ProtoField.new("Dword Count", "pcie.cq.dwlen", ftypes.UINT16, nil, base.DEC)
+f.cq_data    = ProtoField.new("Data", "pcie.cq.data", ftypes.UINT64, nil, base.HEX)
+
+function pcie_proto.dissector(buffer, pinfo, tree)
+	pinfo.cols.protocol = "PCIe TLP (Xilinx)"
+	local subtree = tree:add(pcie_proto, buffer(0, buffer:len()))
+
+	local tcap_subtree = subtree:add(buffer(0,6), "TLP Capture Header")
+	tcap_subtree:add(f.tcap_dir,  buffer(0,1), buffer(0,1):bitfield(0,2))
+--	tcap_subtree:add(f.tcap_rsvd, buffer(0,2), buffer(0,2):bitfield(2,14))
+	tcap_subtree:add(f.tcap_sec,  buffer(2,4))
+
+	local cq_subtree = subtree:add(buffer(6, buffer:len()-6), "Transaction Layer Packet (CQ)")
+	cq_subtree:add(f.cq_addr,    buffer( 6,8))
+	cq_subtree:add(f.cq_at,      buffer(13,1), buffer(13,1):bitfield(6, 2))
+--	cq_subtree:add(f.cq_rsvd0,   buffer(14,1), buffer(14,1):bitfield(0, 1))
+	cq_subtree:add(f.cq_attr,    buffer(14,1), buffer(14,1):bitfield(1, 3))
+	cq_subtree:add(f.cq_tc,      buffer(14,1), buffer(14,1):bitfield(4, 3))
+	cq_subtree:add(f.cq_barap,   buffer(14,2), buffer(14,2):bitfield(7, 6))
+	cq_subtree:add(f.cq_barid,   buffer(15,1), buffer(15,1):bitfield(5, 3))
+	cq_subtree:add(f.cq_tf,      buffer(16,1))
+	cq_subtree:add(f.cq_tag,     buffer(17,1))
+	cq_subtree:add(f.cq_reqid,   buffer(18,2))
+--	cq_subtree:add(f.cq_rsvd1,   buffer(20,1), buffer(20,1):bitfield(0, 1))
+	cq_subtree:add(f.cq_reqtype, buffer(20,1), buffer(20,1):bitfield(1, 4))
+	cq_subtree:add(f.cq_dwlen,   buffer(20,2), buffer(20,2):bitfield(5,11))
+
+	local cqdata_subtree = subtree:add(buffer(22, buffer:len()-22), "TLP data")
+	cqdata_subtree:add(f.cq_data, buffer(22,buffer:len()-22))
+end
+
+-- CC: Completer Completion Descriptor Format
 -- |       0       |       1       |       2       |       3       |
 -- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 -- |          Requester ID         |R|P| CS  |     Dword count     |
@@ -60,7 +134,7 @@ f.tcap_ts   = ProtoField.new("Timestamp", "pcie.tcap.timestamp", ftypes.BYTES)
 -- CS: Completion Status
 -- L: Locked Read Completion
 
--- Requester Request (RQ) Descriptor Format for Memory, I/O, and Atomic Op Requests
+-- RQ: Requester Request Descriptor Format for Memory, I/O, and Atomic Op Requests
 -- |       0       |       1       |       2       |       3       |
 -- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 -- |                       Address [63:32]                         |
@@ -76,7 +150,7 @@ f.tcap_ts   = ProtoField.new("Timestamp", "pcie.tcap.timestamp", ftypes.BYTES)
 -- P: Poisoned Request
 -- AT: Address Type
 
--- Requester Completion (RC) Descriptor Format
+-- RC: Requester Completion Descriptor Format
 -- |       0       |       1       |       2       |       3       |
 -- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 -- |          Requester ID         |R|P| CS  |     Dword count     |
@@ -96,71 +170,6 @@ f.tcap_ts   = ProtoField.new("Timestamp", "pcie.tcap.timestamp", ftypes.BYTES)
 -- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 -- |Device/Function|      Tag      |
 -- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-
-f.tlp_rsvd0   = ProtoField.new("Reserved0", "pcie.tlp.reserved0", ftypes.UINT8, nil, base.NONE)
--- f.tlp_fmt     = ProtoField.new("Packet Format", "pcie.tlp.format", ftypes.UINT8, nil, base.HEX)
-local TLPPacketFormat = {
-	[0] = "MRd_3DW_NO_DATA",
-	[1] = "MRd_4DW_NO_DATA",
-	[2] = "MWr_3DW_DATA",
-	[3] = "MWr_4DW_DATA"
-}
-f.tlp_fmt = ProtoField.uint8("pcie.tlp.format", "Packet Format", base.DEC, TLPPacketFormat)
-
--- f.tlp_type    = ProtoField.new("Packet Type", "pcie.tlp.pkttype", ftypes.UINT8, nil, base.HEX)
-local TLPPacketType = {
-	[ 0] = "MEMORY_RW",
-	[ 4] = "Cfg0_RW",
-	[10] = "Cpl"
-}
-f.tlp_type = ProtoField.uint8("pcie.tlp.pkttype", "Packet Type", base.DEC, TLPPacketType)
-
-f.tlp_rsvd1   = ProtoField.new("Reserved1", "pcie.tlp.reserved1", ftypes.UINT8, nil, base.NONE)
-f.tlp_tclass  = ProtoField.new("Tclass", "pcie.tlp.tclass", ftypes.UINT8, nil, base.HEX)
-f.tlp_rsvd2   = ProtoField.new("Reserved2", "pcie.tlp.reserved2", ftypes.UINT8, nil, base.NONE)
-f.tlp_digest  = ProtoField.new("Digest", "pcie.tlp.digest", ftypes.UINT8, nil, base.HEX)
-f.tlp_poison  = ProtoField.new("Poison", "pcie.tlp.poison", ftypes.UINT8, nil, base.HEX)
-f.tlp_attr    = ProtoField.new("Attr", "pcie.tlp.attr", ftypes.UINT8, nil, base.HEX)
-f.tlp_rsvd3   = ProtoField.new("Reserved3", "pcie.tlp.reserved3", ftypes.UINT8, nil, base.NONE)
-f.tlp_length  = ProtoField.new("Length", "pcie.tlp.length", ftypes.UINT8, nil, base.HEX)
-f.tlp_reqid   = ProtoField.new("Request ID", "pcie.tlp.reqid", ftypes.UINT8, nil, base.HEX)
-f.tlp_tag     = ProtoField.new("Tag", "pcie.tlp.tag", ftypes.UINT8, nil, base.HEX)
-f.tlp_lastbe  = ProtoField.new("LastBE", "pcie.tlp.lastbe", ftypes.UINT8, nil, base.HEX)
-f.tlp_firstbe = ProtoField.new("FirstBE", "pcie.tlp.firstbe", ftypes.UINT8, nil, base.HEX)
-f.tlp_addr    = ProtoField.new("Address", "pcie.tlp.addr", ftypes.UINT8, nil, base.HEX)
-f.tlp_rsvd4   = ProtoField.new("Reserved4", "pcie.tlp.reserved4", ftypes.UINT8, nil, base.NONE)
-
-function pcie_proto.dissector(buffer, pinfo, tree)
-	pinfo.cols.protocol = "PCIe TLP (Xilinx)"
-	local subtree = tree:add(pcie_proto, buffer(0, buffer:len()))
-
-	local tcap_subtree = subtree:add(buffer(0,6), "TLP Capture Header")
-	tcap_subtree:add(f.tcap_ver,  buffer(0,1), buffer(0,1):bitfield(0,3))
-	tcap_subtree:add(f.tcap_dir,  buffer(0,1), buffer(0,1):bitfield(3,2))
-	tcap_subtree:add(f.tcap_rsvd, buffer(0,1), buffer(0,1):bitfield(5,3))
-	tcap_subtree:add(f.tcap_ts,   buffer(1,5))
-
-	local tlp_subtree = subtree:add(buffer(6, buffer:len()-6), "Transaction Layer Packet")
-	tlp_subtree:add(f.tlp_rsvd0,   buffer( 6,1), buffer( 6,1):bitfield(0, 1))
-	tlp_subtree:add(f.tlp_fmt,     buffer( 6,1), buffer( 6,1):bitfield(1, 2))
-	tlp_subtree:add(f.tlp_type,    buffer( 6,1), buffer( 6,1):bitfield(3, 5))
-	tlp_subtree:add(f.tlp_rsvd1,   buffer( 7,1), buffer( 7,1):bitfield(0, 1))
-	tlp_subtree:add(f.tlp_tclass,  buffer( 7,1), buffer( 7,1):bitfield(1, 3))
-	tlp_subtree:add(f.tlp_rsvd2,   buffer( 7,1), buffer( 7,1):bitfield(4, 4))
-	tlp_subtree:add(f.tlp_digest,  buffer( 8,1), buffer( 8,1):bitfield(0, 1))
-	tlp_subtree:add(f.tlp_poison,  buffer( 8,1), buffer( 8,1):bitfield(1, 1))
-	tlp_subtree:add(f.tlp_attr,    buffer( 8,1), buffer( 8,1):bitfield(2, 2))
-	tlp_subtree:add(f.tlp_rsvd3,   buffer( 8,1), buffer( 8,1):bitfield(4, 2))
-	tlp_subtree:add(f.tlp_length,  buffer( 8,2), buffer( 8,2):bitfield(6,10))
-	tlp_subtree:add(f.tlp_reqid,   buffer(10,2), buffer(10,2):bitfield(0,16))
-	tlp_subtree:add(f.tlp_tag,     buffer(12,1), buffer(12,1):bitfield(0, 8))
-	tlp_subtree:add(f.tlp_lastbe,  buffer(13,1), buffer(13,1):bitfield(0, 4))
-	tlp_subtree:add(f.tlp_firstbe, buffer(13,1), buffer(13,1):bitfield(4, 4))
-	tlp_subtree:add(f.tlp_addr,    buffer(14,4), buffer(14,4):bitfield(0,30))
-	tlp_subtree:add(f.tlp_rsvd4,   buffer(17,1), buffer(17,1):bitfield(6, 2))
-
-end
 
 DissectorTable.get("udp.port"):add(14198, pcie_proto)
 
